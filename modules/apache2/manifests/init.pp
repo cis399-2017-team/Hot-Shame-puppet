caf7ebded9ecdd918385fe2ea916b46497a76a45
Holden <holdenoullette@gmail.com>
2017-07-05 12:22:33 -0700

    fixed typo that might allow for installation of apache2 now

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -16,7 +16,7 @@ class apache2 {
 		owner => root,
 		group => root,
 		mode => 644,
-		source => 'puppet://modules/apache2/apache2.conf,
+		source => 'puppet://modules/apache2/apache2.conf',
 		require => Package[apache2],
 		notify => Service[apache2],
 	}
