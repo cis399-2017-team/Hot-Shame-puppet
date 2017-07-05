c8579e4738b8123b7ba505ffc3aef806bb25c655
Holden <holdenoullette@gmail.com>
2017-07-05 12:49:19 -0700

    there's no apache2 package in RedHat, only httpd

--- modules/apache2/manifests/init.pp
+++ modules/apache2/manifests/init.pp
@@ -1,23 +1,23 @@
 class apache2 {
 	
-	package { 'apache2':
+	package { 'httpd':
 		ensure => latest,
 	}
 	
-	service { 'apache2':
-		subscribe => File[apacheconfig],
-		require => Package[apache2],
+	service { 'httpd':
+		subscribe => File["apacheconfig"],
+		require => Package["httpd"],
 		ensure => 'running',
 		enable => 'true',
 	}
 
 	file { 'apacheconfig':
-		name => '/etc/apache2/apache2.conf',
+		name => '/etc/httpd/conf/httpd.conf',
 		owner => root,
 		group => root,
 		mode => 644,
-		source => 'puppet://modules/apache2/apache2.conf',
-		require => Package[apache2],
-		notify => Service[apache2],
+		source => 'puppet://modules/apache2/httpd.conf',
+		require => Package["httpd"],
+		notify => Service["httpd"],
 	}
 }
